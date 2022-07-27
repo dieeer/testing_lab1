71cfc0919ba6d784d4728d96e37c3bb5a4b74c42
justin reid <hi@justinre.id>
2022-07-27 15:21:30 +0100

    completed all bar further

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,3 +1,18 @@
+calendar = {
+    1: "January", 
+    2: "February", 
+    3: "March", 
+    4: "April", 
+    5: "May",
+    6: "June", 
+    7: "July", 
+    8: "August", 
+    9: "September",
+    10: "October"
+}
+
+
+
 def return_10():
     return 10
 
@@ -22,5 +37,9 @@ def join_string(string1, string2):
 def add_string_as_number(result1, result2):
     return int(result1) + int(result2)
 
-def number_to_full_month_name(result):
-    return result[1]
+def number_to_full_month_name(month):
+    return calendar[month]
+
+def number_to_short_month_name(month):
+    short_name = number_to_full_month_name(month)
+    return short_name[0:3]
