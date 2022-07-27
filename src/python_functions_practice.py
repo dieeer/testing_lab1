0b41c89a11ecacab3c000c6681e976c889f8bf89
justin reid <hi@justinre.id>
2022-07-27 14:25:06 +0100

    completed til fullmonth number

---
+++ src/python_functions_practice.py
@@ -0,0 +1,26 @@
+def return_10():
+    return 10
+
+def add(number1 , number2):
+    return number1 + number2
+
+def subtract(number1, number2):
+    return number1 - number2
+
+def multiply(number1, number2):
+    return number1 * number2
+
+def divide(number1, number2):
+    return number1 / number2
+
+def length_of_string(test_string):
+    return len(test_string)
+
+def join_string(string1, string2):
+    return string1 + string2
+
+def add_string_as_number(result1, result2):
+    return int(result1) + int(result2)
+
+def number_to_full_month_name(result):
+    return result[1]
